63ecf4aab6191e3220249d4f5d15341e1157643d
Ilario Del Sorbo <delsorboilario@gmail.com>
2022-09-16 16:55:00 +0200

    add rules text

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -18,9 +18,40 @@ class Rules extends Component {
                     Rules
                 </p>
 
-                <div style={{ width: '100%' }}>
-                    <p style={{ fontSize: 18, color: 'white' }}>
-                        Welcome to Wizards Arena!
+                <div style={{ width: '100%', flexDirection: 'column' }}>
+                    <p style={{ fontSize: 18, color: 'white', lineHeight: 1.2, marginBottom: 10 }}>
+                        Welcome to Wizards Arena! In this article we will explain the mechanism of this experimental project.<br /><br />
+                    </p>
+
+                    <p style={{ fontSize: 21, color: 'white', lineHeight: 1.2, marginBottom: 15 }}>
+                        WIZARD
+                    </p>
+
+                    <p style={{ fontSize: 18, color: 'white', lineHeight: 1.2 }}>
+                    Each wizard is an NFT with an unique combination of traits, and it's also a mighty fighter in the Arena!<br /><br />
+                    Each wizard has this 7 stats: HP, Defense, Attack, Damage, Spells, Resistance, and Weakness. Let's analyze them quickly:<br /><br />
+                    - HP. How much damage your wizard will be able to absorb before losing the battle<br /><br />
+                    - Defense. Your dodging bonus to avoid the opponent's wizard spell, opposed by his Attack (and some bonuses or penalties)<br /><br />
+                    - Attack. Your bonus to hit the opponent wizard with your spells, opposed by his Defense (and some bonuses or penalties)<br /><br />
+                    - Damage. The points subtracted from your opponent's HP after the hit<br /><br />
+                    - Spells. Shots available to your wizard, each with one of the 6 elements associated.<br /><br />
+                    - Resistance. You can halve the damage you suffer from an element, with the right Resistance!<br /><br />
+                    - Weakness. You can double the damage your opponent suffer from your spells if you choose the right element. But be careful, You have a Weakness too!<br/><br />
+                    Each of these stats derives from its traits, we are not going to tell you how the algorithm behind stats creation works, but for istance we can tell you that Devil Skin gives much more HP than Lich Skin, but way less Attack!
+                    </p>
+
+                    <p style={{ fontSize: 21, color: 'white', lineHeight: 1.2, marginTop: 20, marginBottom: 15 }}>
+                        TOURNAMENT
+                    </p>
+
+                    <p style={{ fontSize: 18, color: 'white', lineHeight: 1.2, marginBottom: 10 }}>
+                        There is going to be a tournament every week or two, participation is optional but it's the only way to win KDA and improve your wizard.<br /><br />
+                        The tournament is simple, you pay the registration in KDA and the algorithm will do the rest.<br /><br />
+                        The tournament is divided into a predetermined number of rounds, each round will grant a medal for the winner (this information will always visible on the NFT page).
+                        At the end of the tournament rounds the wizard with more medals from that tournament will win the grand prize! (in the event of a tie, the prize will be divided equally)<br /><br />
+                        Participation will grant a random bonus that will increase HP or defense or attack or damage to all the wizards involved.<br /><br />
+                        What does the algorithm do? after signing up for the tournament, the algorithm matches the participating wizards randomly, and then, based on their stats and some nice calculations, the fight takes place.<br /><br />
+                        When all the fights are over, it awards medals to the winning wizards, and then waits for the next round.<br /><br /><br />
                     </p>
                 </div>
             </div>
