24775214e8e15aa28a3a2bbf9e27a1826c158068
Ilario Del Sorbo <delsorboilario@gmail.com>
2022-11-10 10:46:18 +0100

    add colore diverso se sei l'owner nella schermata torneo

--- src/components/common/NftCardTournament.js
+++ src/components/common/NftCardTournament.js
@@ -7,7 +7,7 @@ import '../../css/NftCard.css'
 
 class NftCardTournament extends Component {
 	render() {
-		const { item, history, width, reveal } = this.props
+		const { item, history, width, reveal, account } = this.props
 
 		//console.log(item);
 
@@ -18,6 +18,11 @@ class NftCardTournament extends Component {
 		}
 
 		//console.log(totalMedals);
+
+		let isMine = false
+		if (account.account && account.account === item.owner) {
+			isMine = true
+		}
 
 		return (
 			<div
@@ -33,19 +38,19 @@ class NftCardTournament extends Component {
 				<div style={{ justifyContent: 'space-between', width, height: 55, alignItems: 'center' }}>
 
 					<div style={{ width: '100%', justifyContent: 'space-between', alignItems: 'center', marginTop: 5 }}>
-						<p style={{ color: 'white', fontSize: 19, marginLeft: 10, lineHeight: 1 }}>
+						<p style={{ color: isMine ? "gold" : "white", fontSize: 19, marginLeft: 10, lineHeight: 1 }}>
 							{item.name}
 						</p>
 					</div>
 
 					<div style={{ width: '100%', alignItems: 'center', justifyContent: 'flex-end', marginTop: 5, marginRight: 10 }}>
 						<IoMedalOutline
-							color="white"
+							color={isMine ? "gold" : "white"}
 							size={22}
 							style={{ marginRight: 8 }}
 						/>
 
-						<p style={{ color: 'white', fontSize: 20, lineHeight: 1 }}>
+						<p style={{ color: isMine ? "gold" : "white", fontSize: 20, lineHeight: 1 }}>
 							{totalMedals}
 						</p>
 					</div>
@@ -57,9 +62,9 @@ class NftCardTournament extends Component {
 }
 
 const mapStateToProps = (state) => {
-	const { reveal } = state.mainReducer
+	const { reveal, account } = state.mainReducer
 
-	return { reveal }
+	return { reveal, account }
 }
 
 export default connect(mapStateToProps)(NftCardTournament);
