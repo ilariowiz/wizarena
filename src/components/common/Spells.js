4854ba0c2ef458476f15a323866b3c1f2f71f2c1
Ilario Del Sorbo <delsorboilario@gmail.com>
2022-12-09 09:19:08 +0100

    add new spells

--- src/components/common/Spells.js
+++ src/components/common/Spells.js
@@ -172,6 +172,179 @@ const allSpells = [
 		condition: { name: 'Slow', effect: "malus_1_atk", pct: 35 },
 		element: 'Wind'
 	},
+    {
+		name: "Earth Tremor",
+		atkBase: 4,
+		dmgBase: 3,
+		condition: { name: 'Slow 2', effect: "malus_3_atk", pct: 90 },
+		element: 'Earth'
+	},
+	{
+		name: "Earthquake",
+		atkBase: 3,
+		dmgBase: 4,
+		condition: { name: 'Shock', effect: "skip", pct: 40 },
+		element: 'Earth'
+	},
+	{
+		name: "Flesh to Stone",
+		atkBase: 5,
+		dmgBase: 7,
+		condition: {},
+		element: 'Earth'
+	},
+	{
+		name: "Erupting Earth",
+		atkBase: 6,
+		dmgBase: 6,
+		condition: {},
+		element: 'Earth'
+	},
+	////// SUN
+	{
+		name: "Sacred Flame",
+		atkBase: 6,
+		dmgBase: 6,
+		condition: {},
+		element: 'Sun'
+	},
+	{
+		name: "Sunbeam",
+		atkBase: 5,
+		dmgBase: 7,
+		condition: { name: 'Confuse', effect: "skip", pct: 20 },
+		element: 'Sun'
+	},
+	{
+		name: "Dawn",
+		atkBase: 4,
+		dmgBase: 3,
+		condition: { name: 'Blind', effect: "skip", pct: 50 },
+		element: 'Sun'
+	},
+	{
+		name: "Solar Faith",
+		atkBase: 3,
+		dmgBase: 4,
+		condition: { name: 'Exhaust', effect: "malus_4_atk", pct: 60 },
+		element: 'Sun'
+	},
+	////// SPIRIT
+	{
+		name: "Cursed Whispers",
+		atkBase: 3,
+		dmgBase: 4,
+		condition: { name: 'Fear', effect: "malus_4_atk", pct: 55 },
+		element: 'Spirit'
+	},
+	{
+		name: "Summon Spirit",
+		atkBase: 7,
+		dmgBase: 5,
+		condition: {},
+		element: 'Spirit'
+	},
+	{
+		name: "Mental Prison",
+		atkBase: 4,
+		dmgBase: 3,
+		condition: { name: 'Paralyze 2', effect: "skip", pct: 40 },
+		element: 'Spirit'
+	},
+	{
+		name: "Wail of spirits",
+		atkBase: 6,
+		dmgBase: 7,
+		condition: {},
+		element: 'Spirit'
+	},
+	/////// UNDEAD
+	{
+		name: "Summon Undead",
+		atkBase: 6,
+		dmgBase: 6,
+		condition: {},
+		element: 'Undead'
+	},
+	{
+		name: "Whiter",
+		atkBase: 3,
+		dmgBase: 4,
+		condition: { name: 'Poison 3', effect: "skip", pct: 40 },
+		element: 'Undead'
+	},
+	{
+		name: "Circle of Death",
+		atkBase: 7,
+		dmgBase: 5,
+		condition: {},
+		element: 'Undead'
+	},
+	{
+		name: "Decay",
+		atkBase: 4,
+		dmgBase: 3,
+		condition: { name: 'Exhaust', effect: "malus_4_atk", pct: 60 },
+		element: 'Undead'
+	},
+	/////// WATER
+	{
+		name: "Tsunami",
+		atkBase: 6,
+		dmgBase: 6,
+		condition: {},
+		element: 'Water'
+	},
+	{
+		name: "Drown",
+		atkBase: 3,
+		dmgBase: 4,
+		condition: { name: 'Exhaust', effect: "malus_4_atk", pct: 60 },
+		element: 'Water'
+	},
+	{
+		name: "Maelstrom",
+		atkBase: 7,
+		dmgBase: 5,
+		condition: {},
+		element: 'Water'
+	},
+	{
+		name: "Watery Sphere",
+		atkBase: 4,
+		dmgBase: 3,
+		condition: { name: 'Paralyze 2', effect: "skip", pct: 40 },
+		element: 'Water'
+	},
+	/////// Psycho
+	{
+		name: "Nightmare",
+		atkBase: 6,
+		dmgBase: 6,
+		condition: {},
+		element: 'Psycho'
+	},
+	{
+		name: "Fear",
+		atkBase: 3,
+		dmgBase: 4,
+		condition: { name: 'Fear', effect: "malus_4_atk", pct: 55 },
+		element: 'Psycho'
+	},
+	{
+		name: "Madness",
+		atkBase: 7,
+		dmgBase: 5,
+		condition: {},
+		element: 'Psycho'
+	},
+	{
+		name: "Command",
+		atkBase: 4,
+		dmgBase: 3,
+		condition: { name: 'Paralyze 2', effect: "skip", pct: 40 },
+		element: 'Psycho'
+	}
 ]
 
 export default allSpells
