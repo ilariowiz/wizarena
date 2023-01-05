121dc8e1fcfeb15cd878d9b917974eb35a933113
Ilario Del Sorbo <delsorboilario@gmail.com>
2023-01-05 12:39:46 +0100

    fix bug t name

--- src/components/common/ConvertMedalName.js
+++ src/components/common/ConvertMedalName.js
@@ -3,8 +3,12 @@ const convertMedalName = (abbr) => {
     let torneo = abbr.split("_")[0]
     let round = abbr.split("_")[1]
 
-    torneo = `tournament ${torneo.split("")[1]}`
-    round = `round ${round.split("")[1]}`
+    //console.log(torneo, round);
+
+    torneo = `tournament ${torneo.replace("t", "")}`
+    round = `round ${round.replace("r","")}`
+
+    //console.log(torneo, round);
 
     return { torneo, round }
 }
