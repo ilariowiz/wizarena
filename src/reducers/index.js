5c320d1d1467e00e74cb8610165c8c871ff4eebc
Ilario Del Sorbo <delsorboilario@gmail.com>
2023-01-26 09:29:51 +0100

    add reducer equipment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import MainReducer from './MainReducer';
 import SalesReducer from './SalesReducer';
 import RankReducer from './RankReducer'
+import EquipmentReducer from './EquipmentReducer'
 
 const rootRed = combineReducers({
 	mainReducer: MainReducer,
 	salesReducer: SalesReducer,
-	rankReducer: RankReducer
+	rankReducer: RankReducer,
+	equipmentReducer: EquipmentReducer
 });
 
 export default rootRed;
