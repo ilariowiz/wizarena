a1a11948b57f2ea191146b7d3d1fc6ead59cd27f
Ilario Del Sorbo <delsorboilario@gmail.com>
2023-03-02 15:46:34 +0100

    add all titles

--- src/components/common/LeagueTitle.js
+++ src/components/common/LeagueTitle.js
@@ -9,6 +9,26 @@ const titles = {
         img: cup_gold,
         textColor: 'gold'
     },
+    "1822": {
+        title: "The Twelve League 2nd place",
+        img: cup_silver,
+        textColor: '#c0c0c0'
+    },
+    "1361": {
+        title: "The Twelve League 3rd place",
+        img: cup_bronze,
+        textColor: '#cd7f32'
+    },
+    "1280": {
+        title: "The Twelve League 4th place",
+        img: medal,
+        textColor: '#cd7f32'
+    },
+    "1614": {
+        title: "The Twelve League 5th place",
+        img: medal,
+        textColor: '#cd7f32'
+    },
     "1871": {
         title: "The Twelve League 6th place",
         img: medal,
@@ -16,6 +36,31 @@ const titles = {
     },
     "854": {
         title: "The Twelve League 7th place",
+        img: medal,
+        textColor: "#cd7f32"
+    },
+    "1117": {
+        title: "The Twelve League 8th place",
+        img: medal,
+        textColor: "#cd7f32"
+    },
+    "1443": {
+        title: "The Twelve League 9th place",
+        img: medal,
+        textColor: "#cd7f32"
+    },
+    "1379": {
+        title: "The Twelve League 10th place",
+        img: medal,
+        textColor: "#cd7f32"
+    },
+    "2047": {
+        title: "The Twelve League 11th place",
+        img: medal,
+        textColor: "#cd7f32"
+    },
+    "94": {
+        title: "The Twelve League 12th place",
         img: medal,
         textColor: "#cd7f32"
     },
