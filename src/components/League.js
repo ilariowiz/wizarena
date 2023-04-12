a32b4531f42b77f24fc8ef4a2dfb684cebbb7876
Ilario Del Sorbo <delsorboilario@gmail.com>
2023-04-12 13:48:49 +0200

    fix horizontal scroll

--- src/components/League.js
+++ src/components/League.js
@@ -270,7 +270,7 @@ class League extends Component {
         const { boxW } = getBoxWidth(isMobile)
 
         return (
-            <div style={{ flexDirection: 'column', width: boxW, marginTop: 5, padding: !isMobile ? 25 : 15, overflow: 'auto' }}>
+            <div style={{ flexDirection: 'column', width: boxW, marginTop: 5, padding: !isMobile ? 25 : 15, overflowY: 'auto', overflowX: 'hidden' }}>
 
                 <div style={{ marginBottom: 10, alignItems: 'center' }}>
                     <p style={{ fontSize: 32, color: 'white', marginRight: 10 }}>
