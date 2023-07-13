1bfc8cd7c820afd1124591046eeb81fb27f81803
Ilario Del Sorbo <delsorboilario@gmail.com>
2023-07-13 18:48:06 +0200

    add max for ranges

--- src/actions/MainActions.js
+++ src/actions/MainActions.js
@@ -425,9 +425,28 @@ export const loadAllNftsIds = (chainId, gasPrice = DEFAULT_GAS_PRICE, gasLimit, 
 
 					let totalCountNfts = final.length
 
+					let maxStats = { hp: 0, defense: 0, attack: 0, damage: 0, speed: 0 }
+
 					final.map(i => {
 						i.level = i.level.int
+
+						if (i.hp.int > maxStats.hp) {
+							maxStats['hp'] = i.hp.int
+						}
+						if (i.defense.int > maxStats.defense) {
+							maxStats['defense'] = i.defense.int
+						}
+						if (i.attack.int > maxStats.attack) {
+							maxStats['attack'] = i.attack.int
+						}
+						if (i.damage.int > maxStats.damage) {
+							maxStats['damage'] = i.damage.int
+						}
+						if (i.speed.int > maxStats.speed) {
+							maxStats['speed'] = i.speed.int
+						}
 					})
+
 
 					const ranges = dispatch(calcRanges(maxStats))
 					//console.log(ranges);
