5b98cf8e610783166529c9646d7f6a4d9736971a
Ilario Del Sorbo <delsorboilario@gmail.com>
2023-07-26 10:45:07 +0200

    get fight from calcFight

--- src/components/DoFight.js
+++ src/components/DoFight.js
@@ -5,6 +5,7 @@ import DotLoader from 'react-spinners/DotLoader';
 import { doc, updateDoc, collection, setDoc, increment } from "firebase/firestore";
 import { firebasedb } from './Firebase';
 import Rainbow from 'rainbowvis.js'
+import fight from './common/CalcFight'
 import { getColorTextBasedOnLevel } from './common/CalcLevelWizard'
 import getBoxWidth from './common/GetBoxW'
 import allSpells from './common/Spells'
@@ -133,334 +134,37 @@ class DoFight extends Component {
 
                         this.player2InitialHp = this.player2.hp
 
-                        this.startFight(this.player1, this.player2)
+
+                        fight(this.player1, this.player2, undefined, (history, winner) => {
+                            //console.log(history, winner);
+
+                            this.setState({ winner, loading: false }, () => {
+                                this.history = history
+
+                                //console.log(this.history);
+                                //console.log(winner);
+
+                                let attaccante = this.player1.id === winner ? this.player1 : this.player2
+                                let difensore = this.player1.id === winner ? this.player2 : this.player1
+
+                                this.updateFirebase(sfida, attaccante, difensore, this.history, sfida.fightsStart)
+
+                                this.indexShow = 0
+
+                                setTimeout(() => {
+                                    this.showFight()
+                                }, 600)
+                            })
+
+                        })
                     }
-
                 })
-
-
 			}
 			else {
 				this.setState({ error: '404', loading: false })
 			}
 		})
 	}
-
-
-    startFight(s1, s2, callback) {
-        let s1copy = Object.assign({}, s1)
-        let s2copy = Object.assign({}, s2)
-        s1copy['condizione'] = {}
-        s2copy['condizione'] = {}
-
-        let iniziativa1 = Math.floor(Math.random() * 15) + 1; //da 1 a 20
-        iniziativa1 = iniziativa1 + s1copy.speed
-
-        let iniziativa2 = Math.floor(Math.random() * 15) + 1; //da 1 a 20
-        iniziativa2 = iniziativa2 + s2copy.speed
-
-        //console.log(iniziativa1, iniziativa2);
-
-        if (iniziativa1 > iniziativa2) {
-            this.turno(s1copy, s2copy)
-        }
-        else if (iniziativa1 < iniziativa2) {
-            this.turno(s2copy, s1copy)
-        }
-        //uguali, controlliamo chi ha la speed più alta
-        else {
-            if (s1copy.speed > s2copy.speed) {
-                this.turno(s1copy, s2copy)
-            }
-            else if (s1copy.speed < s2copy.speed) {
-                this.turno(s2copy, s1copy)
-            }
-            else {
-                //se hanno anche le speed uguali allora random
-                let iniziativaRnd = Math.floor(Math.random() * 10) + 1; //da 1 a 10
-                if (iniziativaRnd <= 5) {
-                    this.turno(s1copy, s2copy)
-                }
-                else {
-                    this.turno(s2copy, s1copy)
-                }
-
-            }
-        }
-    }
-
-    convertConditionName(name) {
-        //console.log(name);
-        if (name.includes("Poison")) {
-            return "is poisoned"
-        }
-        else if (name.includes("Burn")) {
-            return "is burned"
-        }
-        else if (name.includes("Confuse")) {
-            return "is confused"
-        }
-        else if (name.includes("Freeze")) {
-            return "is frozen"
-        }
-        else if (name.includes("Slow")) {
-            return "is slowed"
-        }
-        else if (name.includes("Paralyze")) {
-            return "is paralyzed"
-        }
-        else if (name.includes("Shock")) {
-            return "is shocked"
-        }
-        else if (name.includes("Blind")) {
-            return "is blinded"
-        }
-        else if (name.includes("Exhaust")) {
-            return "is exhausted"
-        }
-        else if (name.includes("Fear")) {
-            return "is frightened"
-        }
-        else if (name.includes("Disease")) {
-            return "is sickened"
-        }
-        else if (name.includes("Harden") || name.includes("Protection")) {
-            return "is less effective"
-        }
-        else if (name.includes("Curse")) {
-            return "is cursed"
-        }
-    }
-
-    convertConditionNamePositive(name) {
-        if (name.includes("Poison")) {
-            return "is no longer poisoned"
-        }
-        else if (name.includes("Burn")) {
-            return "is no longer burned"
-        }
-        else if (name.includes("Confuse")) {
-            return "is no longer confused"
-        }
-        else if (name.includes("Freeze")) {
-            return "is no longer frozen"
-        }
-        else if (name.includes("Slow")) {
-            return "is no longer slowed"
-        }
-        else if (name.includes("Paralyze")) {
-            return "is no longer paralyzed"
-        }
-        else if (name.includes("Shock")) {
-            return "is no longer shocked"
-        }
-        else if (name.includes("Blind")) {
-            return "is no longer blinded"
-        }
-        else if (name.includes("Exhaust")) {
-            return "is no longer exhausted"
-        }
-        else if (name.includes("Fear")) {
-            return "is no longer frightened"
-        }
-        else if (name.includes("Disease")) {
-            return "is no longer sickened"
-        }
-        else if (name.includes("Harden") || name.includes("Protection")) {
-            return "is effective again"
-        }
-        else if (name.includes("Curse")) {
-            return "is no longer cursed"
-        }
-    }
-
-    turno(attaccante, difensore) {
-
-        //console.log(attaccante, difensore);
-
-        let malus = 0
-        let malusTipo = ''
-        let skipTurn = false
-
-        let desc = ""
-
-        if (attaccante.condizione.name) {
-
-            //console.log(attaccante.condizione);
-
-            if (attaccante.condizione.effect.includes("skip")) {
-                let checkSkip = Math.floor(Math.random() * 100) + 1; //da 1 a 100
-                //console.log(checkSkip, attaccante.condizione.pct);
-                if (checkSkip <= attaccante.condizione.pct) {
-                    const textCondizione = this.convertConditionName(attaccante.condizione.name)
-                    desc = `${attaccante.name} ${textCondizione}! Skip his turn! `
-                    //console.log("condizione skip", desc);
-                    skipTurn = true
-                }
-                else {
-                    const textCondizione = this.convertConditionNamePositive(attaccante.condizione.name)
-                    desc = `${attaccante.name} ${textCondizione}! `
-                    //console.log("condizione superata", desc);
-                    attaccante.condizione = {}
-                }
-            }
-            else {
-                let checkCondizione = Math.floor(Math.random() * 100) + 1; //da 1 a 100
-
-                if (attaccante.condizione.pct < checkCondizione) {
-                    const textCondizione = this.convertConditionNamePositive(attaccante.condizione.name)
-                    desc = `${attaccante.name} ${textCondizione}! `
-                    //console.log("condizione superata", desc);
-                    attaccante.condizione = {}
-                }
-                else {
-                    const infoMalus = attaccante.condizione.effect.split("_")
-                    malus = infoMalus[1]
-                    malusTipo = infoMalus[2]
-                    const textCondizione = this.convertConditionName(attaccante.condizione.name)
-                    desc = `${attaccante.name} ${textCondizione}! `
-                    //console.log("condizione malus", desc);
-                }
-            }
-        }
-
-        if (skipTurn) {
-            this.fineTurno(attaccante, difensore, desc, false)
-        }
-        else {
-            const spellSelectedAtk = attaccante.spellSelected
-            let atkAttaccante = attaccante.attack + spellSelectedAtk.atkBase
-
-            if (malusTipo && malusTipo === "atk") {
-                atkAttaccante -= parseInt(malus)
-            }
-
-            let tiro = Math.floor(Math.random() * 20) + 1; //da 1 a 20
-            let atkTot = tiro + atkAttaccante
-
-            let hasDebolezza = false
-            let hasResistenza = false
-
-            let difesaDif = difensore.defense
-            if (difensore.condizione.name) {
-                if (difensore.condizione.effect.includes("def")) {
-                    const infoMalus = difensore.condizione.effect.split("_")
-                    difesaDif -= parseInt(infoMalus[1])
-                }
-            }
-
-            //console.log(tiro);
-
-            //se atktot è maggiore o uguale alla difesa, oppure il tiro è un 20 oppure il tiro non è un uno
-            if ((atkTot >= difesaDif || tiro >= 18) && tiro !== 1) {
-                //console.log("hit");
-
-                const dannoBase = attaccante.damage
-                let dannoSpell = dannoBase + attaccante.spellSelected.dmgBase
-
-                if (malusTipo === "dmg") {
-                    dannoSpell -= parseInt(malus)
-                }
-
-                let danno = dannoSpell + (Math.floor(Math.random() * 5) - 2)
-
-                if (attaccante.element.toLowerCase() === difensore.weakness) {
-                    danno *= 2
-                    hasDebolezza = true
-                }
-                else if (attaccante.element.toLowerCase() === difensore.resistance) {
-                    danno = Math.floor(danno/2)
-                    hasResistenza = true
-                }
-
-                difensore.hp -= danno
-
-                if (difensore.hp > 0) {
-                    if (attaccante.spellSelected.condition.name) {
-                        const textCondizione = this.convertConditionName(attaccante.spellSelected.condition.name)
-                        desc = `${difensore.name} ${textCondizione}! `
-                        difensore.condizione = attaccante.spellSelected.condition
-                    }
-
-                    if (!hasDebolezza && !hasResistenza) {
-                        if (desc.includes("no longer")) {
-                            desc = `${desc}${attaccante.name} hits ${difensore.name} and deals ${danno} damage (${difensore.name} has ${difensore.hp} hp left).`
-                        }
-                        else {
-                            desc = `${attaccante.name} hits ${difensore.name} and deals ${danno} damage (${difensore.name} has ${difensore.hp} hp left). ${desc}`
-                        }
-                    }
-                    else if (hasDebolezza) {
-                        if (desc.includes("no longer")) {
-                            desc = `${desc}${attaccante.name} hits ${difensore.name}, who is weak to ${attaccante.element}, and deals ${danno} damage (${difensore.name} has ${difensore.hp} hp left).`
-                        }
-                        else {
-                            desc = `${attaccante.name} hits ${difensore.name}, who is weak to ${attaccante.element}, and deals ${danno} damage (${difensore.name} has ${difensore.hp} hp left). ${desc}`
-                        }
-                    }
-                    else if (hasResistenza) {
-                        if (desc.includes("no longer")) {
-                            desc = `${desc}${attaccante.name} hits ${difensore.name}, who is ${attaccante.element} resistant, and deals ${danno} damage (${difensore.name} has ${difensore.hp} hp left).`
-                        }
-                        else {
-                            desc = `${attaccante.name} hits ${difensore.name}, who is ${attaccante.element} resistant, and deals ${danno} damage (${difensore.name} has ${difensore.hp} hp left). ${desc}`
-                        }
-                    }
-
-                    this.fineTurno(attaccante, difensore, desc, false)
-                }
-                else {
-                    desc = `${desc}${attaccante.name} hits and ${difensore.name} is K.O., the round is over!`
-                    this.fineTurno(attaccante, difensore, desc, true)
-                }
-            }
-            else {
-                desc = `${desc}${attaccante.name} misses ${difensore.name}`
-                this.fineTurno(attaccante, difensore, desc, false)
-            }
-        }
-    }
-
-    fineTurno(attaccante, difensore, desc, end) {
-        //console.log(attaccante, difensore, desc, end);
-        const { sfida } = this.props
-
-        //console.log(newH);
-
-        let currentHp1;
-        let currentHp2;
-        if (attaccante.name === this.player1.name) {
-            currentHp1 = attaccante.hp
-            currentHp2 = difensore.hp
-        }
-        else {
-            currentHp1 = difensore.hp
-            currentHp2 = attaccante.hp
-        }
-
-        const obj = { desc, player1CurrentHp: currentHp1, player2CurrentHp: currentHp2 }
-        this.history.push(obj)
-
-        //console.log(currentHp1, currentHp2);
-
-        if (end) {
-            //console.log(attaccante, difensore, desc, end);
-
-            this.setState({ loading: false, winner: attaccante.name }, () => {
-
-                this.updateFirebase(sfida, attaccante, difensore, this.history, sfida.fightsStart)
-
-                this.indexShow = 0
-
-                setTimeout(() => {
-                    this.showFight()
-                }, 600)
-            })
-        }
-        else {
-            this.turno(difensore, attaccante)
-        }
-    }
 
     async updateFirebase(sfida, attaccante, difensore, history, fightsStart) {
 
@@ -523,7 +227,7 @@ class DoFight extends Component {
                 this.indexShow += 1
                 this.turnTimeout = setTimeout(() => {
                     this.showFight()
-                }, 4000)
+                }, 3500)
             }
             else {
                 this.setState({ isEnd: true })
@@ -544,6 +248,8 @@ class DoFight extends Component {
 
             historyShow.splice(0, 0, item)
         })
+
+        //console.log(historyShow);
 
         this.setState({ historyShow, isEnd: true })
     }
@@ -652,7 +358,7 @@ class DoFight extends Component {
 
                     <div style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
                         <p style={{ color: mainTextColor, fontSize: 12, marginBottom: 3 }}>
-                            hp {item.hp} - def {item.defense} - atk {item.attack + item.spellSelected.atkBase} - dmg {item.damage + item.spellSelected.dmgBase} - speed {item.speed}
+                            hp {item.hp} - def {item.defense} - atk {item.attack + item.spellSelected.atkBase + item['upgrades-spell'].attack.int} - dmg {item.damage + item.spellSelected.dmgBase + item['upgrades-spell'].damage.int} - speed {item.speed}
                         </p>
                         {
                             !isMobile &&
@@ -709,10 +415,10 @@ class DoFight extends Component {
             boxW = 1050
         }
 
-        //console.log(historyShow);
+        //console.log(historyShow, this.player1InitialHp);
 
-        const player1CurrentHp = historyShow.length > 0 ? historyShow[0].player1CurrentHp : this.player1InitialHp
-        const player2CurrentHp = historyShow.length > 0 ? historyShow[0].player2CurrentHp : this.player2InitialHp
+        const player1CurrentHp = historyShow.length > 0 ? historyShow[0][`hp_${this.player1.id}`] : this.player1InitialHp
+        const player2CurrentHp = historyShow.length > 0 ? historyShow[0][`hp_${this.player2.id}`] : this.player2InitialHp
 
         const widthSide = (boxW / 2) - 20
 
