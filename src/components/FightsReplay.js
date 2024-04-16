3f43be43f615fce54ff02401de8c946c49c76624
Ilario Del Sorbo <delsorboilario@gmail.com>
2024-04-16 15:44:56 +0200

    fix if not dark mode

--- src/components/FightsReplay.js
+++ src/components/FightsReplay.js
@@ -290,6 +290,11 @@ class FightsReplay extends Component {
     }
 
     hexToRgb(hex) {
+
+        if (hex === "white") {
+            hex = "#FFFFFF"
+        }
+
         return ['0x' + hex[1] + hex[2] | 0, '0x' + hex[3] + hex[4] | 0, '0x' + hex[5] + hex[6] | 0];
     }
 
