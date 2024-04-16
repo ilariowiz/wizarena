2e6dd189942c92a1cb758795d01964df6aefebee
Ilario Del Sorbo <delsorboilario@gmail.com>
2024-04-16 10:05:46 +0200

    remove useless

--- src/components/common/GetBoxW.js
+++ src/components/common/GetBoxW.js
@@ -1,12 +1,8 @@
 const getBoxWidth = (isMobile) => {
 
-	//const headerW = document.getElementById("headerbox") ? document.getElementById("headerbox").offsetWidth : 0
-	//console.log(headerW);
-
 	const innerW = window.innerWidth
 
 	const padding = (innerW * 12 / 100) / 2
-	//let boxW = Math.floor(window.innerWidth - headerW - padding)
 	let boxW = Math.floor(innerW - (padding * 2))
 
 	//console.log(isMobile, window.innerWidth, boxW);
