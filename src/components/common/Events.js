815587422af938d820c2cab54b4345c658a341f8
Ilario Del Sorbo <delsorboilario@gmail.com>
2024-04-20 18:43:33 +0200

    Revert "fix events bonus"
    This reverts commit 16afa980b22dca59610e368dc42efb257e5d121a.

--- src/components/common/Events.js
+++ src/components/common/Events.js
@@ -3,84 +3,84 @@ const allEvents = [
 		name: "Eruption",
         descriptionOn: "An eruption! It's raining fire and stones! The power of Fire greatly increases",
         descriptionOff: "Looks like the eruption is over for now...",
-        bonus: { attack: 0, damage: 10 },
+        bonus: { attack: 15, damage: 15 },
         elements: "fire"
 	},
 	{
 		name: "Earthquake",
         descriptionOn: "A violent earthquake is shaking the region! The power of Earth greatly increases",
         descriptionOff: "Looks like the earthquake is over for now...",
-        bonus: { attack: 0, damage: 10 },
+        bonus: { attack: 15, damage: 15 },
         elements: "earth"
 	},
 	{
 		name: "Thunderstorm",
         descriptionOn: "The weather suddenly changes, a thunderstorm is coming! The power of Thunder greatly increases",
         descriptionOff: "Looks like the storm is over for now...",
-        bonus: { attack: 0, damage: 10 },
+        bonus: { attack: 15, damage: 15 },
         elements: "thunder"
 	},
 	{
 		name: "Avalanche",
         descriptionOn: "A distant rumble shakes the air. An avalanche is coming! The power of Ice greatly increases",
         descriptionOff: "Looks like the avalanche is over for now...",
-        bonus: { attack: 0, damage: 10 },
+        bonus: { attack: 15, damage: 15 },
         elements: "ice"
 	},
 	{
 		name: "Deadly Fumes",
         descriptionOn: "A deadly fumes rises from the wast marshes. Breathing is getting harder and harder! The power of Acid greatly increases",
         descriptionOff: "A warm wind carries away the deadly fumes for now...",
-        bonus: { attack: 0, damage: 10 },
+        bonus: { attack: 15, damage: 15 },
         elements: "acid"
 	},
 	{
 		name: "Deadly Fog",
         descriptionOn: "The fog thickens and feels like it's seeping into your bones, you suddenly feel tired. The power of Spirit greatly increases",
         descriptionOff: "The fog seems to have loosened its grip for now...",
-        bonus: { attack: 0, damage: 10 },
+        bonus: { attack: 15, damage: 15 },
         elements: "spirit"
 	},
 	{
 		name: "Flood",
         descriptionOn: "the rain suddenly increases and the battlefield is about to be submerged! The power of Water greatly increases",
         descriptionOff: "It looks like it has stopped raining for now...",
-        bonus: { attack: 0, damage: 10 },
+        bonus: { attack: 15, damage: 15 },
         elements: "water"
 	},
 	{
 		name: "Numbing Whispers",
         descriptionOn: "Dark whispers echo around you, fear gripping your heart and locking your limbs. The power of Psycho greatly increases",
         descriptionOff: "The whispers are gone for now...",
-        bonus: { attack: 0, damage: 10 },
+        bonus: { attack: 15, damage: 15 },
         elements: "psycho"
 	},
 	{
 		name: "Sunburst",
         descriptionOn: "Brilliant sunlight flashes in the arena. The power of Sun greatly increases",
         descriptionOff: "The flashes seem to have disappeared for now...",
-        bonus: { attack: 0, damage: 10 },
+        bonus: { attack: 15, damage: 15 },
         elements: "sun"
 	},
 	{
 		name: "Life Drain",
         descriptionOn: "A presence hovers in the air and you feel life leaving you. The power of Undead greatly increases",
         descriptionOff: "That dark feeling is gone for now...",
-        bonus: { attack: 0, damage: 10 },
+        bonus: { attack: 15, damage: 15 },
         elements: "undead"
 	},
 	{
 		name: "Darkness",
         descriptionOn: "Suddenly any source of light is dimmed and everything becomes dark and deadly sinister. The power of Dark greatly increases",
         descriptionOff: "Seems to have returned a bit of light...",
-        bonus: { attack: 0, damage: 10 },
+        bonus: { attack: 15, damage: 15 },
         elements: "dark"
 	},
 	{
 		name: "Tornado",
         descriptionOn: "A tornado suddenly appears! The power of Wind greatly increases",
         descriptionOff: "Looks like the tornado is over for now...",
-        bonus: { attack: 0, damage: 10 },
+        bonus: { attack: 15, damage: 15 },
         elements: "wind"
 	},
 ]
