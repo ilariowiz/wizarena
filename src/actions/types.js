1869d5f240f3e170b9f313234c651a38f29bbc90
Ilario Del Sorbo <delsorboilario@gmail.com>
2024-06-05 10:13:31 +0200

    update node

--- src/actions/types.js
+++ src/actions/types.js
@@ -12,8 +12,8 @@ export const REVEAL_CAP = 3084
 
 export const RING_MINT_PRICE = 4.0
 
-export const NODE_URL = 'api.chainweb.com'
-//export const NODE_URL = 'chainweb.ecko.finance'
+//export const NODE_URL = 'api.chainweb.com'
+export const NODE_URL = 'chainweb.ecko.finance'
 export const ADMIN_ADDRESS = 'k:90f45921e0605560ace17ca8fbbe72df95ba7034abeec7a8a7154e9eda7114eb'
 export const CLERIC_MINT_ADDRESS = "k:9ca8b0b628eb386edafcb66cb90cfd79f349433502e1c1dece1fa097f6801250"
 export const CONTRACT_NAME = 'wiz-arena'//'wiz-arena-test3'
