848169a491e5addc05c561a37bb90aa9c60c655f
ron <128203911+wizard-ron17@users.noreply.github.com>
2025-11-10 16:00:20 -0500

    Update Explorer Link
    Update KDA explorer link for community fork

--- src/actions/MainActions.js
+++ src/actions/MainActions.js
@@ -89,7 +89,7 @@ export const setNetworkUrl = (netId, chainId) => {
 
 		let url;
 		if (netId === TEST_NET_ID) {
-			url = `https://api.testnet.chainweb.com/chainweb/0.0/${TEST_NET_ID}/chain/${chainId}/pact`;
+			url = `https://api.testnet.chainweb-community.org/chainweb/0.0/${TEST_NET_ID}/chain/${chainId}/pact`;
 		}
 		else if (netId === MAIN_NET_ID) {
 			//url = `https://api.chainweb.com/chainweb/0.0/${MAIN_NET_ID}/chain/${chainId}/pact`;
