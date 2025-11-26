68ddb672ed499cb8e7dc5d8bfcd50856bc4cd241
Ilario Del Sorbo <delsorboilario@gmail.com>
2025-11-26 19:55:22 +0100

    prevent navigation from play menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -734,7 +734,8 @@ class Header extends Component {
 				style={styles.boxBtnMenuPlay}
 				onClick={(e) => {
 					e.preventDefault()
-					this.props.history.replace(`/${goto}`)
+					return
+					//this.props.history.replace(`/${goto}`)
 				}}
 			>
 				<img
